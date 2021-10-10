287ce1b8036e6fc0211bf21985e940af00d7eaf5
danielterletzkiy <danielterletzkiy@gmail.com>
2021-10-10 22:56:22 +0200

    - Download with file structure - Secret download

--- background.js
+++ background.js
@@ -6,11 +6,16 @@ chrome.runtime.onMessage.addListener(
             function (screenshotUrl) {
                 sendResponse({imgSrc: screenshotUrl});
 
+                chrome.downloads.setShelfEnabled(false)
+
                 const parser = document.createElement('a');
                 parser.href = sender.url;
-
-                const filename = `${parser.hostname}_${parser.pathname.replaceAll('/', '')}_${(new Date().getDay())}-${(new Date().getMonth() + 1)}---${(new Date().getFullYear())}-${(new Date().getHours())}-${(new Date().getMinutes())}-${(new Date().getSeconds())}.png`
+                console.log(parser.pathname)
+                const filename = `${parser.hostname}/${parser.pathname !== '/' ? parser.pathname.replaceAll('/', '') : 'root'}/${(new Date().getDate())}-${(new Date().getMonth() + 1)}-${(new Date().getFullYear())}---${(new Date().getHours())}-${(new Date().getMinutes())}-${(new Date().getSeconds())}.png`
                 console.log(filename)
+
+                chrome.downloads.onChanged.addListener(onChanged);
+
                 chrome.downloads.download({url: screenshotUrl, filename}, function (downloadId) {
                     console.log("download begin, the downId is:" + downloadId);
                 });
@@ -19,6 +24,13 @@ chrome.runtime.onMessage.addListener(
         return true;
     }
 );
+
+function onChanged({state}) {
+    if (state && state.current !== 'in_progress') {
+        chrome.downloads.onChanged.removeListener(onChanged);
+        chrome.downloads.setShelfEnabled(true)
+    }
+}
 
 /*chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: ".js"});
